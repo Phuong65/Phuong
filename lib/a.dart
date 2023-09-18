42a002f8b131bcc59119a7d77be756b547fc4700
Phuong65 <145210443+Phuong65@users.noreply.github.com>
2023-09-18 16:11:06 +0700

    unfinished

---
+++ lib/a.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class a extends StatefulWidget {
+  const a({super.key});
+
+  @override
+  State<a> createState() => _aState();
+}
+
+class _aState extends State<a> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Text"),),);
+  }
+}
